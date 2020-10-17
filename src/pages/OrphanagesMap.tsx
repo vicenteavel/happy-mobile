523dca3c1bd205da72bd4db1c0dd4f5c7519dd46
Vicente Avelino <vic5ente@gmail.com>
2020-10-17 10:45:42 -0300

    add orphanage details page and create orphanage page

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
 import { Feather } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
+
 
 import mapMarket from '../images/map-marker.png';
 
@@ -12,8 +14,11 @@ export default function OrphanagesMap() {
 
 	function handleNavigateToOrphanageDetails() {
 		navigation.navigate('OrphanageDetails');
-
 	}
+
+  function handleNavigateToCreateOrphanage() {
+    navigation.navigate('SelectMapPosition');
+  }
 
 	return (
 		<View style={styles.container}>
@@ -49,10 +54,10 @@ export default function OrphanagesMap() {
       <View style={styles.footer}>
         <Text style={styles.footerText}>2 orfanatos encontrados</Text>
 
-        <TouchableOpacity style={styles.createOrphanageButton} onPress={() => {}}>
+        <RectButton style={styles.createOrphanageButton} onPress={handleNavigateToCreateOrphanage}>
           <Feather name="plus" size={20} color='#fff'/>
           
-        </TouchableOpacity>
+        </RectButton>
       </View>
 
     </View>
